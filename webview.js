769c4392d9c20deb3af1fddef0d76292a898ec6c
Sunghyun Song <sunghyun.song@sunghyunso-ltm1.internal.salesforce.com>
2020-03-07 16:21:40 -0800

    quip is weird

--- webview.js
+++ webview.js
@@ -1,37 +1,16 @@
 module.exports = (Franz) => {
   const getMessages = function getMessages() {
-    const muteSelector = '.DQy0Rb';
-
-    // get unread messages
+    
     let directCount = 0;
-    document.querySelectorAll('.eM5l9e.FVKzAb').forEach((node) => {
-      // Hangouts Chat overrides the muted indicator when there is a direct mention
-      if (!node.closest('content[role="listitem"]').querySelector(muteSelector)) {
-        directCount += 1;
-      }
-    });
     let indirectCount = 0;
-    document.querySelectorAll('.PL5Wwe.H7du2 .t5F5nf').forEach((node) => {
-      if (!node.closest('content[role="listitem"]').querySelector(muteSelector)) {
-        indirectCount = +1;
-      }
-    });
-    indirectCount -= directCount;
 
-    // set Franz badge
+    this.directCount = document.getElementsByClassName('unread-notifications badge-count')[0].innerText
+    
     Franz.setBadge(directCount, indirectCount);
+    
   };
 
-  document.addEventListener('click', (e) => {
-    const { tagName, target, href } = e.target;
-
-    if (tagName === 'A' && target === '_blank') {
-      e.preventDefault();
-      e.stopImmediatePropagation();
-      window.open(href);
-    }
-  });
-
-  // check for new messages every second and update Franz badge
+  
   Franz.loop(getMessages);
+  
 };
